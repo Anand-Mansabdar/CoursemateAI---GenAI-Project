37c5342ffd938aeb74740058733cf9fa70f7f1e9
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-13 22:09:25 +0530

    Testing DocumentLoaders(TextLoader) to open documents(txt file)

---
+++ Document Loaders/test.py
@@ -0,0 +1,13 @@
+from langchain_community.document_loaders import TextLoader
+
+data = TextLoader("notes.txt", encoding="utf-8")
+# <langchain_community.document_loaders.text.TextLoader object at 0x00000176A63F0500> Generates this kind of output
+# print(data)
+
+documents = data.load()
+print(documents)
+print(documents[0].page_content)
+"""
+Produces an output in the form of a list
+[Document(metadata={'source': 'notes.txt'}, page_content='Hello how are you \n\nI want to see what can I do and also I need your help \nplease help me')]
+"""
