cfcff21f222963bda82789580cbb1468c22216a6
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-13 22:17:04 +0530

    Implementing TextLoader in main.py file

--- main.py
+++ main.py
@@ -1,10 +1,24 @@
 from dotenv import load_dotenv
 from langchain_mistralai import ChatMistralAI
+from langchain_community.document_loaders import TextLoader
+from langchain_core.prompts import ChatPromptTemplate
 
 load_dotenv()
 
+data = TextLoader("Document Loaders/notes.txt")
+documents = data.load()
+
+template = ChatPromptTemplate.from_messages(
+  [
+    ("system", "You are an intelligent AI that summarizes the text"), 
+    ("human", "{data}")
+  ]
+)
+
 model = ChatMistralAI(model="mistral-small-2506")
 
-response = model.invoke("Hello")
+prompt = template.format_messages(data = documents[0].page_content)
+
+response = model.invoke(prompt)
 
 print(response.content)
