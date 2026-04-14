cf2ae7929a643ad6b9000a5c748061acfab42af2
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-14 20:04:35 +0530

    Implementing Recursive Splitting

--- Document Loaders/webpage.py
+++ Document Loaders/webpage.py
@@ -1,11 +1,23 @@
 from langchain_community.document_loaders import WebBaseLoader
+from langchain_text_splitters import RecursiveCharacterTextSplitter
 
 url = "https://www.apple.com/in/macbook-pro/"
 
 data = WebBaseLoader(url)
+
+splitter = RecursiveCharacterTextSplitter(chunk_size=1000, chunk_overlap=10)
 
 document = data.load()
 
 print(document)
 print(len(document))
 print(document[0].page_content)
+
+print("-----------------------------------------")
+
+chunks = splitter.split_documents(document)
+for i in chunks:
+  print(i.page_content)
+  print("----------------------")
+  
+print(len(chunks))
