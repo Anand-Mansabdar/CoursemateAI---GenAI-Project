7212e9469aacafeb640246ce2034c6f270e2cb67
Anand-Mansabdar <dummystudy040105@gmail.com>
2026-04-16 00:24:02 +0530

    Creating a Vector Database using Croma

---
+++ Vector Store/db.py
@@ -0,0 +1,20 @@
+from langchain_community.vectorstores import Chroma
+from langchain_mistralai import MistralAIEmbeddings
+from dotenv import load_dotenv
+from langchain_core.documents import Document
+
+load_dotenv()
+
+documents = [
+    Document(page_content="Python is widely used in Artificial Intelligence.", metadata={"source": "AI_book"}),
+    Document(page_content="Pandas is used for data analysis in Python.", metadata={"source": "DataScience_book"}),
+    Document(page_content="Neural networks are used in deep learning.", metadata={"source": "DL_book"}),
+]
+
+embedding_model = MistralAIEmbeddings(model="mistral-embed")
+
+vector_store = Chroma.from_documents(
+  documents=documents,
+  embedding=embedding_model,
+  persist_directory="chroma_db"
+)
